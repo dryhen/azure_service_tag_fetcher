f8d2c3ccb4f2fc1da131115c694885626fbf6951
dryhen <75749022+dryhen@users.noreply.github.com>
2024-07-09 09:34:17 -0400

    Opted for using Requests library and writing logs to a logfile

--- az_service_fetcher.py
+++ az_service_fetcher.py
@@ -1,10 +1,12 @@
 import sys
 import os
-import urllib.request
+import requests
+import logging
 import json
 import ipaddress
 from datetime import datetime, timedelta
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename="az_tags_retrieval.log")
 # Attempt to assemble the link based on the date, this method is good unless holiday's introduce differences in the pattern
 # or if the base URL changes for some reason
 # An alternative would be to scrape the webpage for the URL, but that also relies on Microsoft not changing the layout of the page
@@ -13,17 +15,27 @@ curr_mon = today - timedelta(days=today.weekday())
 # Try to pull the JSON dated with the latest Monday
 try:
     calculated_filename = f"ServiceTags_Public_{curr_mon.strftime('%Y%m%d')}.json"
-    urllib.request.urlretrieve(f'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/{calculated_filename}', calculated_filename)
-except:
+    r = requests.get(f'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/{calculated_filename}', allow_redirects=True)
+    r.raise_for_status()  # raises exception when not a 2xx response
+    if r.status_code != 204:
+        with open('service_tags.json', 'w') as output:
+            output.write(json.dumps(r.json(), indent=4))
+except Exception as e:
+    logging.warning(f'- (LINE 21) {e}')
     # Try to pull the JSON dated with last week's Monday
     try:
         prev_mon = curr_mon - timedelta(days=curr_mon.weekday(), weeks=1)
         calculated_filename = f"ServiceTags_Public_{prev_mon.strftime('%Y%m%d')}.json"
-        urllib.request.urlretrieve(f'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/{calculated_filename}', calculated_filename)
-    except:
-        print("Failed to retreive most recent Public Service Tag Information")
+        r = requests.get(f'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/{calculated_filename}', allow_redirects=True)
+        r.raise_for_status()  # raises exception when not a 2xx response
+        if r.status_code != 204:
+            with open('service_tags.json', 'w') as output:
+                output.write(json.dumps(r.json(), indent=2))
+    except Exception as e2:
+        logging.warning(f'- (LINE 32) {e2}')
+        logging.info("- Failed to retreive most recent Public Service Tag Information")
         exit(1)
-    print("Failed to retreive this week's Public Service Tag Information\n(New list not published or URL generated incorrectly)\nUsing previous week's data...")
+    logging.info("- Failed to retreive this week's Public Service Tag Information (New list not published or URL generated incorrectly) Using previous week's data...")
 
 argument = sys.argv[1]
 try:
@@ -38,9 +50,9 @@ except:
         exit(1)
 
 try:
-    service_tags = json.load(open(calculated_filename))
+    service_tags = json.load(open('service_tags.json'))
 except:
-    print(f'Failed to open {calculated_filename}')
+    print(f'Failed to open service_tags.json')
     exit(1)
 
 # Because the address can be found in multiple places in the JSON, currently this will spit out multiple things saying similar things
@@ -53,7 +65,7 @@ for st in service_tags["values"]:
                 match = {"name": st["name"], "cidr": cidr}
                 print(f'CIDR Found for {argument}:\n {match["name"]} - {match["cidr"]}')
 try:
-    os.remove(calculated_filename)
+    os.remove('service_tags.json')
 except:
-    print(f'Failed to delete {calculated_filename}')
+    print(f'Failed to delete service_tags.json')
     exit(1)
